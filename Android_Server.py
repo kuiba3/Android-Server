97cea7e464ff474ef2ec1d8907dce32801e5e22e
kuiba <1428328230@qq.com>
2018-11-12 12:23:58 +0800

    联系人的数据库存储完成

--- Android_Server.py
+++ Android_Server.py
@@ -1,10 +1,15 @@
 from socketserver import (TCPServer as TCP,StreamRequestHandler as SRH)
 from time import ctime
+import pymysql
 
 HOST = ''
 PORT = 6543
 
 ADDR = (HOST, PORT)
+
+def database():
+    db = pymysql.connect('localhost','root','12345','server')
+    return db
 
 class MyRequestHandler(SRH):
     def handle(self):
@@ -17,7 +22,8 @@ class MyRequestHandler(SRH):
         if data_type != "keyword_message":
             data = self.request.recv(1024).strip().decode()
             datadict = eval(data)
-            print(data)
+            #print(data)
+            print(datadict)
 
         else:
             lengthstr = self.request.recv(1024).strip().decode()
@@ -28,16 +34,60 @@ class MyRequestHandler(SRH):
             for i in range(0,length):
                 data = self.request.recv(4 * 1024).strip().decode()
                 datadict = eval(data)
-                print(data)
+                #print(data)
+                print(datadict)
+
+        if data_type == "keyword_contact":
+            Contact(datadict)
 
         send_data = (data_type + str(" ok")).encode('utf-8')
         self.request.send(send_data)
         print("send OK :", send_data)
 
+def Contact(data):
+    ## 获取IMEI的值
+    IMEI = data['IMEI']
+
+    ##连接数据库并获取连接对象
+    db = database()
+    cur = db.cursor()
+
+    ## 获取联系人的信息，并查询电话号码在数据库中是否存在
+    ## 如果不存在，添加到数据库
+    ## 如果存在，更新数据库中的联系人信息
+    number = ''
+    for d in data:
+        if d != 'IMEI':
+            name = list(data[d])[0]
+            number = data[d][name]
+
+            selectsql = "select * from contact where IMEI = '%s' and number = '%s' "%(IMEI,number)
+            cur.execute(selectsql)
+            print("rowcount is ",cur.rowcount)
+            if cur.rowcount > 0:
+                updatesql = "update contact set contact_name = '%s' where IMEI = '%s' and number = '%s' " \
+                            %(name,IMEI,number)
+                try:
+                    cur.execute(updatesql)
+                    db.commit()
+                except:
+                    db.rollback()
+            else:
+                insertsql = "insert into contact(IMEI,contact_name,number) values" \
+                            "('%s','%s','%s')" %(IMEI,name,number)
+                try:
+                    cur.execute(insertsql)
+                    db.commit()
+                except:
+                    db.rollback()
+
+
+    db.close()
 
 
 
-tcpServ = TCP(ADDR, MyRequestHandler)
+if __name__ == '__main__':
+    tcpServ = TCP(ADDR, MyRequestHandler)
 
-print('waiting for connection...')
-tcpServ.serve_forever()
+    print('waiting for connection...')
+    tcpServ.serve_forever()
--- test.py
+++ test.py
@@ -1,6 +1,57 @@
-st = '{"刘星":"13212314563"}'
-print(st)
+import pymysql
 
-stdic = eval(st)
-print(stdic)
-print(type(stdic))
+data = {'IMEI': '863952036392072', '0': {'副营长': '13755132542'}, '1': {'马班长': '13874811864'}}
+def database():
+    db = pymysql.connect('localhost','root','12345','server')
+    return db
+def Contact(data):
+    ## 获取IMEI的值
+    IMEI = data['IMEI']
+
+    ##连接数据库并获取连接对象
+    db = database()
+    cur = db.cursor()
+
+    ## 获取联系人的信息，并查询电话号码在数据库中是否存在
+    ## 如果不存在，添加到数据库
+    ## 如果存在，更新数据库中的联系人信息
+    number = ''
+    for d in data:
+        if d != 'IMEI':
+            name = list(data[d])[0]
+            number = data[d][name]
+
+            selectsql = "select * from contact where IMEI = '%s' and number = '%s' "%(IMEI,number)
+            cur.execute(selectsql)
+            print("rowcount is ",cur.rowcount)
+            if cur.rowcount > 0:
+                updatesql = "update contact set contact_name = '%s' where IMEI = '%s' and number = '%s' " \
+                            %(name,IMEI,number)
+                try:
+                    cur.execute(updatesql)
+                    db.commit()
+                except:
+                    db.rollback()
+            else:
+                insertsql = "insert into contact(IMEI,contact_name,number) values" \
+                            "('%s','%s','%s')" %(IMEI,name,number)
+                try:
+                    cur.execute(insertsql)
+                    db.commit()
+                except:
+                    db.rollback()
+
+
+    db.close()
+
+if __name__ == '__main__':
+    '''db = pymysql.connect("localhost","root","12345","server")
+    cur = db.cursor()
+    try:
+        cur.execute("delete from app")
+        db.commit()
+    except:
+        db.rollback()'''
+
+    Contact(data)
+    pass
